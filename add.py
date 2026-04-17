f68f55fe02182a992f379f8a2a87e2e5d6c2fab8
yogesh747 <yogeshborude747@gmail.com>
2026-04-17 14:20:09 +0530

    Multiplication

--- add.py
+++ add.py
@@ -1,6 +1,8 @@
 a = 10
 b = 20
 print("Addition is : ", a+b)
-print("Substraction is :",b-a)
+print("Substraction is :", b-a)
+print("Multiplication is :", a*b)
+
 
 print("Webhook Triggered SUccessfully")
